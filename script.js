7a0ad10e1865a9272aae679792579b36963fa0d2
helenemartin <helenemartin13@hotmail.com>
2017-11-01 19:39:45 +0000

    clean css and fix javascript toggle

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ var c;
 
 window.onload = function() {
   a = document.getElementsByClassName("dropdown")[0];
-  console.log("hello");
 
   b = document.getElementsByClassName("dropdownitem")[0];
   c = document.getElementsByClassName("triangle")[0];
+  b.style.display = 'none'; 
   a.addEventListener("click", function(){
     if (
      b.style.display === 'none'
     ){
+      console.log("hello");
      b.style.display = 'block'; 
-    c.innerHTML = "&#x25BE;";
+    c.innerHTML = "&#x25B2;";
     }
     else {
       b.style.display = 'none'; 
-      c.innerHTML = "&#x25B2;";
+      c.innerHTML = "&#x25BE;";
     }
   });
 };
