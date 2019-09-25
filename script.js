c1ff99f93278d9646d40402e5bcd15c0e708e8f9
helenemartin <helenemartin13@hotmail.com>
2019-09-25 16:25:26 +0100

    add script for speech utterance, add hidden text field class, on click button

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-var a;
-var b;
-var c;
 
-window.onload = function() {
-  a = document.getElementsByClassName("dropdown")[0];
+var utterance = new SpeechSynthesisUtterance();
+utterance.lang = 'en-UK';
+utterance.rate = 1;
 
-  b = document.getElementsByClassName("dropdownitem")[0];
-  c = document.getElementsByClassName("triangle")[0];
-  b.style.display = 'none'; 
-  a.addEventListener("click", function(){
-    if (
-     b.style.display === 'none'
-    ){
-      console.log("hello");
-     b.style.display = 'block'; 
-    c.innerHTML = "&#x25B2;";
-    }
-    else {
-      b.style.display = 'none'; 
-      c.innerHTML = "&#x25BE;";
-    }
-  });
+
+document.getElementById('playbtn').onclick = function(){
+    var text = document.getElementById('textarea').value;
+    utterance.text = text;
+    speechSynthesis.speak(utterance);
 };
+
