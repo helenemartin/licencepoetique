9e3bd7afe4b0699b91de136afa81079d2b3e43d3
helenemartin <helenemartin13@hotmail.com>
2019-11-13 19:40:18 +0000

    randomize voices

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
 var utterance = new SpeechSynthesisUtterance();
-utterance.lang = 'en-UK';
+var lang = ['en-UK', 'fr-FR', 'nl-BE', 'it-IT', 'da-BK', 'fr-BE'];
 utterance.rate = 1;
 
 
 document.getElementById('playbtn').onclick = function(){
     var text = document.getElementById('textarea').value;
     utterance.text = text;
+    var langIndex = Math.floor(Math.random() * lang.length);
+    utterance.lang = lang[langIndex];
+    console.log(lang[langIndex])
     speechSynthesis.speak(utterance);
 };
 
+
+//responsiveVoice.OnVoiceReady = function (){
+    // function choseVoice() {
+    //     var voices = ["Australian Female", "Australian Male", "UK English Male", "Uk English Female"];
+    //     var voiceIndex = Math.floor(Math.random() * voices.length);
+    //     return voices[voiceIndex];
+    // }
